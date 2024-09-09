31619f755719d8e8257e0f0e3aea6a7ce0a34f6b
BeSingularTeam <besingulardemo@gmail.com>
2024-09-09 12:10:39 +0530

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,33 @@
-import React from 'react'
-import HomePage from './pages/HomePage'
-import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import React from 'react';
+import HomePage from './pages/HomePage';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+} from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-     <BrowserRouter>
-     <Routes>
-      <Route path='/' element={<HomePage/>} />
-      <Route path='/login' element={<Login/>} />
-      <Route path='/register' element={<Register/>} />
-     </Routes>
-     </BrowserRouter>
-    </div>
-  )
+    <BrowserRouter>
+      <div>
+        {/* Navigation Links */}
+        <nav>
+          <Link to="/" style={{ margin: '0 10px' }}>HomePage</Link>
+          <Link to="/login" style={{ margin: '0 10px' }}>LoginPage</Link>
+          <Link to="/register" style={{ margin: '0 10px' }}>Register</Link>
+        </nav>
+        {/* Route Definitions */}
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -4,15 +4,21 @@ import './index.css'; // Your custom CSS
 import 'antd/dist/reset.css'; // Import Ant Design styles
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom'; 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const rootElement = document.getElementById('root');
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+if (!rootElement) {
+  console.error('Root element not found');
+} else {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>
+  );
+}
+
 reportWebVitals();
