152c666d7a97fb202e42832630a41e1c2034885b
BeSingularTeam <besingulardemo@gmail.com>
2024-10-10 22:49:14 +0530

    update

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,54 +1,58 @@
 import React from "react";
-import "../styles/Register.css";
+import "../styles/RegiserStyles.css";
 import { Form, Input, message } from "antd";
 import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { showLoading, hideLoading } from "../redux/features/alertSlice";
-
 const Register = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
-
-  const onFinishHandler = async (values) => {
+  //form handler
+  const onfinishHandler = async (values) => {
     try {
       dispatch(showLoading());
-      const res = await axios.post("http://localhost:8800/api/auth/register", values);
+      const res = await axios.post("/api/v1/user/register", values);
       dispatch(hideLoading());
       if (res.data.success) {
-        message.success("Registered Successfully!");
+        message.success("Register Successfully!");
         navigate("/login");
       } else {
         message.error(res.data.message);
       }
     } catch (error) {
       dispatch(hideLoading());
-      console.error(error);
+      console.log(error);
       message.error("Something Went Wrong");
     }
   };
-
   return (
-    <div className="form-container">
-      <Form layout="vertical" onFinish={onFinishHandler} className="register-form">
-        <h3 className="text-center">Register Form</h3>
-        <Form.Item label="Name" name="name" required>
-          <Input type="text" />
-        </Form.Item>
-        <Form.Item label="Email" name="email" required>
-          <Input type="email" />
-        </Form.Item>
-        <Form.Item label="Password" name="password" required>
-          <Input type="password" />
-        </Form.Item>
-        <Link to="/login" className="m-2">
-          Already a user? Login here
-        </Link>
-        <button className="btn btn-primary" type="submit">
-          Register
-        </button>
-      </Form>
-    </div>
+    <>
+      <div className="form-container ">
+        <Form
+          layout="vertical"
+          onFinish={onfinishHandler}
+          className="register-form"
+        >
+          <h3 className="text-center">Register From</h3>
+          <Form.Item label="Name" name="name">
+            <Input type="text" required />
+          </Form.Item>
+          <Form.Item label="Email" name="email">
+            <Input type="email" required />
+          </Form.Item>
+          <Form.Item label="Password" name="password">
+            <Input type="password" required />
+          </Form.Item>
+          <Link to="/login" className="m-2">
+            Already user login here
+          </Link>
+          <button className="btn btn-primary" type="submit">
+            Register
+          </button>
+        </Form>
+      </div>
+    </>
   );
 };
 
